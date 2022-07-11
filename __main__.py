00ba3b0747312008492169a556977f296353c0c8
Charles Paulet <valkheim@protonmail.com>
2022-07-11 18:57:34 +0200

    add repl

--- __main__.py
+++ __main__.py
@@ -1,15 +1,13 @@
-import asmshell.shell
+import os
+import asmshell.cli
 import asmshell.utils
+import asmshell.repl
 
 def show_banner():
     print("Welcome to my asm shell")
 
 show_banner()
-while True:
-    try:
-        user_str = input("> ")
-        asmshell.shell.parse(user_str)
 
-    except KeyboardInterrupt:
-        asmshell.utils.exit()
-
+repl = asmshell.repl.Repl([".q"])
+repl.enable_history(os.getenv('ASMSHELL_HISTORY_FILE', '~/.asms_history'))
+repl.start()
