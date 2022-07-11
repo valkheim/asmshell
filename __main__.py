0827dbf1f28adf7db88cf8fe89f21e5923b58be8
Charles Paulet <valkheim@protonmail.com>
2022-07-11 19:18:58 +0200

    show and suggests commands

--- __main__.py
+++ __main__.py
@@ -3,13 +3,23 @@ import os
 import asmshell.repl
 import asmshell.utils
 
+commands = {
+    ".q": "Quit this program",
+    ".quit": "Quit this program",
+}
 
-def show_banner():
+
+def welcome():
     print("Welcome to my asm shell")
+    print("Commands")
+    for k, v in commands.items():
+        padding = (10 - len(k)) * " "
+        print(f" {k}: {padding}{v}")
 
 
-show_banner()
+if "verbose":
+    welcome()
 
-repl = asmshell.repl.Repl([".q"])
+repl = asmshell.repl.Repl(commands.keys())
 repl.enable_history(os.getenv("ASMSHELL_HISTORY_FILE", "~/.asms_history"))
 repl.start()
