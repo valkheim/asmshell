a81e094ff8686af593ad24dbfa94006c203e3ba8
valkheim <>
2022-07-14 15:56:09 +0200

    display memory commands

--- TODO.md
+++ TODO.md
@@ -1,4 +1,2 @@
-* add .h .help command
 * add instructions manual
-* add more registers and flags like CR*
 * allow file import (and single step?)
--- asmshell/commands.py
+++ asmshell/commands.py
@@ -1,21 +1,22 @@
 import os
 import sys
+from typing import Optional
 
-from . import config, display
+from . import config, display, utils
 
 
-def quit() -> None:
+def quit(cmd: Optional[str] = None) -> None:
     sys.exit()
 
 
-def clear() -> None:
+def clear(cmd: Optional[str] = None) -> None:
     if os.name == "posix":
         os.system("clear")
     elif os.name == "nt":
         os.system("cls")
 
 
-def help() -> None:
+def help(cmd: Optional[str] = None) -> None:
     print("Commands")
     for command_literals, description, _ in config.config.commands:
         grouped_commands = ", ".join(command_literals)
@@ -25,9 +26,32 @@ def help() -> None:
     print()
 
 
-def registers() -> None:
+def registers(cmd: Optional[str] = None) -> None:
     display.show_x86_64_registers()
 
 
-def stack() -> None:
+def stack(cmd: Optional[str] = None) -> None:
     display.show_x86_64_stack()
+
+
+def display_memory_range(cmd: str, length: int) -> None:
+    range = utils.get_memory_range(cmd)
+    range.end = range.start + length
+    mem = config.config.mu.mem_read(range.start, range.end - range.start)
+    utils.hexdump(mem, base=range.start)
+
+
+def db(cmd: str) -> None:
+    display_memory_range(cmd, 1)
+
+
+def dw(cmd: str) -> None:
+    display_memory_range(cmd, 2)
+
+
+def dd(cmd: str) -> None:
+    display_memory_range(cmd, 4)
+
+
+def dq(cmd: str) -> None:
+    display_memory_range(cmd, 8)
