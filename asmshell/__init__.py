b34ee9f8505a9993338fd52bdbd6630835dab30d
valkheim <>
2022-07-15 11:24:51 +0200

    display memory

--- asmshell/__init__.py
+++ asmshell/__init__.py
@@ -21,6 +21,7 @@ config.config.commands = [
     ((".dw",), "Display word values", commands.dw),
     ((".dd",), "Display double-word values", commands.dd),
     ((".dq",), "Display quad-word values", commands.dq),
+    ((".dm",), "Display memory", commands.dm),
     ((".di",), "Display instructions", commands.di),
 ]
 
