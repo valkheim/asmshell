99ddc2f5fa48e2cd798ca2ba3f2f6fcfd51665bc
valkheim <>
2022-09-10 22:56:58 +0200

    fix uc exception (e.g. invalid instruction)

--- asmshell/emulator.py
+++ asmshell/emulator.py
@@ -1,8 +1,11 @@
+import logging
 from typing import Any, Dict
 
 import unicorn
 
 from asmshell import commands, config, registers
+
+logger = logging.getLogger(__name__)
 
 
 def get_state() -> Dict[Any, Any]:
@@ -17,13 +20,16 @@ def get_state() -> Dict[Any, Any]:
 
 
 def emulate(code: bytes) -> None:
-    config.config.mu.mem_write(config.config.emu_base, code)
-    config.config.mu.emu_start(
-        config.config.emu_base, config.config.emu_base + len(code)
-    )
-    config.config.emu_previous_mu.context_restore(
-        config.config.emu_previous_ctx
-    )
-    commands.cmd_registers()
-    commands.cmd_stack()
-    config.config.emu_previous_ctx = config.config.mu.context_save()
+    try:
+        config.config.mu.mem_write(config.config.emu_base, code)
+        config.config.mu.emu_start(
+            config.config.emu_base, config.config.emu_base + len(code)
+        )
+        config.config.emu_previous_mu.context_restore(
+            config.config.emu_previous_ctx
+        )
+        commands.cmd_registers()
+        commands.cmd_stack()
+        config.config.emu_previous_ctx = config.config.mu.context_save()
+    except unicorn.unicorn.UcError as err:
+        logger.exception(err)
